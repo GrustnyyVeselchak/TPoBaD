16ea54d92f003ed8dd7ca06ea131a7156784c599
GrustnyyVeselchak <113785577+GrustnyyVeselchak@users.noreply.github.com>
2025-09-06 19:25:28 +0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import SearchBar from './ui/Search-field'
+
+
+function App() {
+  return (
+    <>
+      <SearchBar/>
+    </>
+  )
+}
+
+export default App
---
+++ src/ui/Search-field.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react"
+
+const SearchField = () => {
+    const [query, setQuery] = useState("")
+    
+    return(
+        <div>
+            <input
+                
+            />
+        </div>
+    )
+}
+
+export default SearchField;
