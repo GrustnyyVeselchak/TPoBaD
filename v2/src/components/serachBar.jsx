d6c053ecea610ec478188849b157dfe67b764347
GrustnyyVeselchak <113785577+GrustnyyVeselchak@users.noreply.github.com>
2025-09-18 16:03:48 +0400

    Паггинация готова!

--- v2/src/components/serachBar.jsx
+++ v2/src/components/serachBar.jsx
@@ -1,37 +1,66 @@
 import axios from "axios"
 import { useEffect } from "react"
 import { useState } from "react"
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { gettingRepos, errorRepos, successRepos } from "../store/reposSlice"
 
 const SearchBar = () => {
     const [ username, setUsername ] = useState('')
-    const dispatch = useDispatch()
+    const [ currentPage, setCurrentPage ] = useState(1) 
+    const [ fetching, setFetching ] = useState(false)
 
-    const handleChange = (event) => {setUsername(event.target.value)}
+    const dispatch = useDispatch()
+    const { repos } = useSelector(state => state.repos)
+
+    const handleChange = (e) => {setUsername(e.target.value)}
     
+    const scrollHandler = (e) => {
+        if(e.target.documentElement.scrollHeight - (e.target.documentElement.scrollTop + window.innerHeight) < 100) {
+            setFetching(true)   
+        }
+    }
+
+    useEffect(() => {
+        document.addEventListener('scroll', scrollHandler)
+        return function() {
+            document.removeEventListener('scroll', scrollHandler)
+        }
+    })
+
     useEffect(() => {
         if( username.trim() === "" ) {
             dispatch(successRepos([]))
+            setCurrentPage(1)
             return
         }
 
         const timer = setTimeout(() => {
             dispatch(gettingRepos())
-            axios
-                .get(`https://api.github.com/users/${username}/repos`)
-                .then(respone => {
-                    dispatch(successRepos(respone.data))
-                })
-                .catch(error => {
-                    console.log(error)
-                    dispatch(errorRepos('Пользователь не найден'))
-                })
+            fetchRepos(username, currentPage)
         }, 1000)
 
         return () => clearTimeout(timer)
 
-    }, [username, dispatch])  
+    }, [username])  
+
+    useEffect(() => {
+        if (fetching) {
+            fetchRepos(username, currentPage)
+        }
+    }, [fetching])
+
+    const fetchRepos = (username, currentPage) => { 
+        axios
+            .get(`https://api.github.com/users/${username}/repos?_limit=10&page=${currentPage}`)
+            .then(respone => {
+                dispatch(successRepos([...repos, ...respone.data]))
+                setCurrentPage( prevState => prevState + 1)
+            })
+            .catch(error => {
+                console.log(error)
+                dispatch(errorRepos('Пользователь не найден'))
+            })
+    }
     
     return(
         <>
