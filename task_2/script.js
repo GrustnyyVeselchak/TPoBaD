431936698f686bba684e50505cd56c8cbd6b5dbb
GrustnyyVeselchak <113785577+GrustnyyVeselchak@users.noreply.github.com>
2025-09-24 23:44:36 +0400

    Первичная верстка + обработка ввода

--- task_2/script.js
+++ task_2/script.js
@@ -1,1 +1,51 @@
-console.log('Hello world')
+const myInput = document.querySelector('input.myInput')
+const myButton = document.querySelector('button.myButton')
+const myOutput = document.querySelector('p.myOutput')
+
+let str = ""
+
+const valitationData = (str) => {
+    if (str === '') {
+        throw new Error("Не ввели данные")
+    }
+
+    let isValid = /^[0-9()]+$/.test(str) //возвращает ture если строка состоит из чисул и знаков скобок
+
+    if (!isValid) {
+        throw new Error("Невалидные данные, пишите только цифры и символы '(', ')' ")
+    }
+}
+
+const customParse = (str) => {
+    let arr = Array.from(str)
+    let i = 0
+    let r = arr.length
+
+    for (i; i<r; i++) {
+        if ( arr[i] === "(") {
+            console.log("опа")
+        } else if ( arr[i] === ")") {
+            console.log("Оппапулички")
+        } else  {
+            console.log(arr[i])
+        }
+    }
+
+    str = arr.join("")
+    return str
+}
+
+myInput.addEventListener('input', (event) => {
+    str = event.target.value.replace(/\s+/g, '').trim()
+})
+
+myButton.addEventListener('click', () => {
+    try {
+        valitationData(str)
+        myOutput.textContent = customParse(str)
+    } catch(err) {
+        console.log("Возникла ошибка" + " " + err.message)
+        myOutput.textContent = "Возникла ошибка" + " " + err.message
+    }
+    
+})
